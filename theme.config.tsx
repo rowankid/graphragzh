99789a418328619639ee6cf4d8d8149720935f04
Wenqi Luo <495872803@qq.com>
2024-07-15 16:18:10 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,11 +7,11 @@ import Banner from './components/Banner'
 
 
 const config: DocsThemeConfig = {
-  logo: <span>GraphRAG 中文网</span>,
+  logo: <span>GraphRAG</span>,
   project: {
-    link: 'https://github.com/liteli1987gmail/graphragzh'
+    link: 'https://github.com/rowankid/graphragzh'
   },
-  docsRepositoryBase: 'https://github.com/liteli1987gmail/graphragzh',
+  docsRepositoryBase: 'https://github.com/rowankid/graphragzh',
   head: () => {
       const { asPath, defaultLocale, locale } = useRouter()
       const { frontMatter } = useConfig()
@@ -48,23 +48,11 @@ const config: DocsThemeConfig = {
   toc: {
     float: true,
     extraContent:(
-      <div>
-        <img src="https://www.aiqbh.com/qun.png" alt="扫我，入群" />
-        <img src="https://pic2.zhimg.com/100/v2-23e6630a548c962582265f27e8967cd1_qhd.jpg" alt="扫我，找书" />
-      </div>
+
     )
   },
   footer: {
-    text: <div><span>MIT {new Date().getFullYear()} © <a href="https://www.langchain.com.cn/" target="_blank">GraphRAG</a>. 跟着langchain学AI应用开发    </span>
-    <span><a href="https://www.r-p-a.com/llm-gpt-kaifa/" target="_blank">    LLM/GPT应用外包开发    |</a></span>
-    <span><a href="https://www.openaidoc.com.cn" target="_blank">    OpenAI 文档    |</a></span>
-    <span><a href="https://www.milvus-io.com" target="_blank">    Milvus 文档    |</a></span>
-    <span><a href="https://www.pinecone-io.com/ " target="_blank">    Pinecone 文档 </a></span>
-    <p>
-      <span><a href="https://www.Langchain.com" target="_blank">    Langchain英文站  </a></span>
-      <span><a href="https://js.langchain.com.cn/docs/">    Langchain JS/TS 文档 </a></span>
-      <a href="https://www.langchain.com.cn"><span><img style={{ display: "inline-block",height: "19px" }} src="https://mbdp01.bdstatic.com/static/landing-pc/img/icon_police.7296bdfd.png" alt="" />  沪ICP备2023014280号-3</span></a></p>
-    </div>
+    text: 
   }
 }
 
